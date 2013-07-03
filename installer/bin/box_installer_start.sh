bdcfb9d70f49bf4118c1064f5654aee431d5d59a
Matthias Strubel <matthias.strubel@aod-rpg.de>
2013-07-03 15:57:37 +0200

    use flag-file to remember if install was run once Fixes #2

--- installer/bin/box_installer_start.sh
+++ installer/bin/box_installer_start.sh
@@ -2,5 +2,12 @@
 
 # Initiates the log facility and starts the installation
 
-syslogd -L -R 192.168.1.2:9999
-/bin/box_installer.sh 2>&1 | logger 
+if [ ! -e /etc/auto_install_done ] ; then
+	syslogd -L -R 192.168.1.2:9999
+	/bin/box_installer.sh 2>&1 | logger 
+	# create the file always
+	touch /etc/auto_install_done
+else
+	echo "not running auto installation because  /etc/auto_install_done exists"
+fi
+
