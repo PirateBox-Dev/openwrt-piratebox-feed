d144cb3025ce620c048644029cc3a2edadad0987
Matthias Strubel <matthias.strubel@aod-rpg.de>
2013-08-07 17:40:36 +0200

    use start-stop daemon to get correct PID and kill process later on

--- files/bin/box_installer_start.sh
+++ files/bin/box_installer_start.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+
+PID=/var/run/auto_syslogd
+
 # Initiates the log facility and starts the installation
 if [  -e /mnt/usb/install/auto_package || !  /etc/init.d/ext enabled ] ; then
-	syslogd -L -R 192.168.1.2:9999
+	start-stop-daemon -b -S -m -p $PID -x syslogd -- -n -L -R 192.168.1.2:9999
 	/bin/box_installer.sh 2>&1 | logger 
 	# create the file always
 	mv /mnt/usb/install/auto_package  /mnt/usb/install/auto_package_done
+	kill $(cat $PID)
 else
 	echo "Does not run because /mnt/usb/install/auto_package  does not exists"
 fi
